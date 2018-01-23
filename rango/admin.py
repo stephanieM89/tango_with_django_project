682508284d7036b2074825e137e3efe382c97cf0
Stephanie Man <2286857m@student.gla.ac.uk>
2018-01-23 23:16:25 +0000

    Updates

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from rango.models import Category, Page
+
+
+# This PageAdmin class inherits from admin.ModelAdmin
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
+
+
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
-
 from rango import views
 
 
@@ -27,4 +25,4 @@ urlpatterns = [
     url(r'about/$', views.about, name='about'),
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
