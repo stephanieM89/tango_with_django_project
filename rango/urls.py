c8d41dc2e9b4fd01813ea91f5723d298d3c1f466
Stephanie Man <2286857m@student.gla.ac.uk>
2018-01-28 20:46:12 +0000

    Chapter 7 updates

--- rango/urls.py
+++ rango/urls.py
@@ -4,5 +4,7 @@ from rango import views
 urlpatterns=[
     url(r'^$', views.index, name='index'),  # Maps the basic URL to the index view in the rango app
     url(r'about/$', views.about, name='about'),  # Maps the URL to the about view in the rango app
+    url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
 ]
